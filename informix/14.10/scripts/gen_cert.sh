d58a9f7aa3a9e5677f75674cb9fe59b7e59bc739
Frank Yoo <fyoo@outlook.com>
2021-08-18 13:08:23 +1000

    [M] env for informix change

--- informix/14.10/scripts/gen_cert.sh
+++ informix/14.10/scripts/gen_cert.sh
@@ -9,5 +9,5 @@ CERT_INFO=`cat client.crt.nohead | tr -d '[:space:]'`
 # Install the cert in SDKMS
 SDKMS_API_KEY=`curl -s -X POST -u ${SDKMS_API_USERNAME}:${SDKMS_API_PASSWORD} https://${SDKMS_API_ENDPOINT}/sys/v1/session/auth | jq -r '.access_token'`
 curl -s -X POST -H "Authorization:Bearer $SDKMS_API_KEY" -d "{\"acct_id\":\"${SDKMS_ACCT_ID}\"}" https://${SDKMS_API_ENDPOINT}/sys/v1/session/select_account
-curl -s -X PATCH -H "Authorization:Bearer $SDKMS_API_KEY" -d "{\"credential\":{\"certificate\":\"$CERT_INFO\"}}" https://${SDKMS_API_ENDPOINT}/sys/v1/apps/${SDKMS_APP_ID}
+curl -s -X PATCH -H "Authorization:Bearer $SDKMS_API_KEY" -d "{\"credential\":{\"certificate\":\"$CERT_INFO\"}}" https://${SDKMS_API_ENDPOINT}/sys/v1/apps/${SDKMS_INFORMIX_APP_ID}
 curl -s -X POST -H "Authorization:Bearer $SDKMS_API_KEY" https://${SDKMS_API_ENDPOINT}/sys/v1/session/terminate
--- informix/14.10/scripts/gen_conf.sh
+++ informix/14.10/scripts/gen_conf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -e "s|<endpoint>|$SDKMS_API_ENDPOINT|g" kmip.json.template | sed -e "s|<keyid>|$SDKMS_KEY_ID|g" > kmip.json
+sed -e "s|<endpoint>|$SDKMS_API_ENDPOINT|g" kmip.json.template | sed -e "s|<keyid>|$SDKMS_INFORMIX_KEY_ID|g" > kmip.json
